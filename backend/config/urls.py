d4e9717feaa885f39bc2f458c536c5ed151e1be4
VishnuDharshan <130656256+VIsHnu1762@users.noreply.github.com>
2026-01-31 15:52:53 +0530

    Fix os import

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+import os
 from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt.views import TokenObtainPairView as BaseTokenObtainPairView
