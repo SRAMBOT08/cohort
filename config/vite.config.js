f986f8b939ace21bb783e8d85b529dc62248660a
Jabbastin Akash <akashrenisha068@gmail.com>
2026-02-04 05:48:15 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ config/vite.config.js
@@ -0,0 +1,26 @@
+              import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/static/frontend/',
+  build: {
+    outDir: './backend/static/frontend',
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/media': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+})
